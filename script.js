38bedba50cec61f05286d01d9cd14da27024dab6
abduuuuuu <abdulmenanmohammed28@gmail.com>
2022-09-01 13:49:09 +0300

    changing the color of the active link in the navigation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//script to handle the stickiness of the header
 const header = document.querySelector("header");
 window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",window.scrollY > 0);
