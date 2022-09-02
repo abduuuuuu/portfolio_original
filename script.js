84dca4a7fca6d7e4af2a913fb37908f471764e9e
abduuuuuu <abdulmenanmohammed28@gmail.com>
2022-09-02 10:48:10 +0300

    making it responsive

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const header = document.querySelector("header");
 window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",window.scrollY > 0);
 })
+const burger = document.getElementsByClassName("menu")[0];
+burger.addEventListener("click",function(){
+    burger.classList.toggle("bx-x");
+    burger.classList.toggle("burger-active");
+    const ul = document.querySelector("ul");
+    ul.classList.toggle("ul-active")
+})
